1064eaa253907a74c30c5140724770a2eba0938e
yoshifumi-yamamoto <toyotama.arank.acekiller@gmail.com>
2022-10-12 22:33:27 +0900

    不要なコードの削除とundefinedになる不具合を修正

--- コード.js
+++ コード.js
@@ -13,45 +13,13 @@ var samples = ['Item number', 'Title', 'Variation details', 'Custom label (SKU)'
 
 // var DATA = [['Item number', 'Title', 'Variation details', 'Custom label (SKU)', 'Available quantity', 'Format', 'Currency', 'Start price', 'Auction Buy It Now price', 'Reserve price', 'Current price', 'Sold quantity', 'Views (future)', 'Watchers', 'Bids', 'Start date', 'End date', 'eBay category 1 name', 'eBay category 1 number', 'eBay category 2 name', 'eBay category 2 number', 'Condition', 'eBay Product ID(ePID)', 'Listing site', 'P:UPC', 'P:EAN', 'P:ISBN'], [353847193474, 'JoJo,s Bizarre Adventure All Star Battle PS3 Japanese version Used', , 'B00BHAF688', 3, 'FIXED_PRICE', 'USD', 25.74, , , 25.74, 1, , 2, , 'Jan-07-22 19:09:09 PST', 'Oct-07-22 20:09:09 PDT', 'Video Games', 139973, , , 'VERY_GOOD', , 'US', , , ], [353864972609, 'Street Fighter Collection Playstation 1 PS1 Sony Japan Capcom 1997 Japanese used', , 'B000069TD7', 1, 'FIXED_PRICE', 'USD', '38.61', , , '38.61', 0, , 9, , 'Jan-19-22 00:57:23 PST', 'Oct-19-22 01:57:23 PDT', 'Video Games', 139973, , , 'GOOD', , 'US', , , ]]
 
-
+// モーダルを開く
 function showModal() {
 
-
-  const researcherGetSheet = SpreadsheetApp.openById(RESEARCHER_GET_SHEET_ID)
-  const cells =  researcherGetSheet.createTextFinder('B000069TD7').findAll()
-  const cell = cells[0].getA1Notation() 
-  console.log(cell)
-  const r = cell.substr(1)
-  console.log(r)
-  const range = 'AD' + r
-  const researcher = researcherGetSheet.getRange(range).getDisplayValue()
-  console.log('researcher')
-  console.log(researcher)
-
-  //データがある最終列を取得
-  const lastCol = researcherGetSheet.getLastColumn();
-
-  console.log('lastCol')
-  console.log(lastCol)
-
-  // SKU列全取得
-  const skus = researcherGetSheet.getSheetByName("出品 年月").getRange(1,4,10,1).getValues();
-  const researchers = researcherGetSheet.getSheetByName("出品 年月").getRange(1,32,10,1).getValues();
-
-
-  const formattedSkus = skus.reduce(function (acc, cur) {
-    return acc.concat(cur);
-  });
-  
-  // リサーチ担当の列を全取得
-  const formattedResearchers = researchers.reduce(function (acc, cur) {
-    return acc.concat(cur);
-  });
-
-  
-
-
+  // 開いているスプレッドシートを取得
   const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+
+  // HTMLファイルを取得
   const output = HtmlService.createTemplateFromFile('index');
   const data = spreadsheet.getSheetByName(SHEET_NAME);
 
@@ -62,37 +30,37 @@ function showModal() {
   spreadsheet.show(html);
 }
 
+// アップロードボタン
 function sendForm(formObject) {
+  
+  // フォームから受け取ったcsvデータ
   const blob = formObject.myFile;
-  console.log('blob')
-  console.log(blob)
-  // const csvText = blob.getDataAsString();
-  // const values = Utilities.parseCsv(csvText)
-  // console.log(values)
   const csvText = blob.getDataAsString("sjis");
   const values = Utilities.parseCsv(csvText);
 
   const ss = SpreadsheetApp.getActive();
   const sheet = ss.getSheetByName(SHEET_NAME);
 
+  // リサーチ担当を取得するシートを取得
   const researcherGetSheet = SpreadsheetApp.openById(RESEARCHER_GET_SHEET_ID)
 
-    //データがある最終列を取得
+  //データがある最終列を取得（上手くいってない）
   const lastCol = researcherGetSheet.getLastColumn();
-
   console.log('lastCol')
   console.log(lastCol)
 
   // SKU列全取得
-  const skus = researcherGetSheet.getSheetByName("出品 年月").getRange(1,4,4500,1).getValues();
-  const researchers = researcherGetSheet.getSheetByName("出品 年月").getRange(1,30,4500,1).getValues();
+  const skus = researcherGetSheet.getSheetByName("出品 年月").getRange(1,4,4999,1).getValues();
+
+  // リサーチ担当列全取得
+  const researchers = researcherGetSheet.getSheetByName("出品 年月").getRange(1,30,4999,1).getValues();
 
 
+  // 二次元配列を一次元配列に変換
   const formattedSkus = skus.reduce(function (acc, cur) {
     return acc.concat(cur);
   });
   
-  // リサーチ担当の列を全取得
   const formattedResearchers = researchers.reduce(function (acc, cur) {
     return acc.concat(cur);
   });
@@ -100,27 +68,26 @@ function sendForm(formObject) {
   // 必要な項目のインデックスを取得
   const indexs = ITEMS.map(function (item) {
     return values[0].indexOf(item)
-})
+  })
 
-// 2次元配列に整形
+  // 2次元配列に整形
   var addValues = []
   values.slice(1).map(function (value){
     const fiteredValues = indexs.map(function (index) {
       return value[index]
     })
-    console.log('fiteredValues')
-    console.log(fiteredValues)
+    console.log('watchers')
+    // console.log(fiteredValues)
+    console.log(fiteredValues[3] !== '0')
     
-    if(fiteredValues[3] !== '0' && fiteredValues[3] !== ''){
+    if(fiteredValues[3] !== '0' && fiteredValues[3] !== '' && fiteredValues[3] !== undefined){
       const skuIndex = formattedSkus.indexOf(fiteredValues[1])
       fiteredValues.push(formattedResearchers[skuIndex])
       addValues.push(fiteredValues)
     }
   })
   
-  console.log(addValues)
-  
   // 既存レコードをクリアし、CSVのレコードを貼り付け
   // clearRecords(RC_ROW, RC_COL, sheet);
-  sheet.getRange(RC_ROW - 1, RC_COL, addValues.length, addValues[0].length).setValues(addValues);
+  sheet.getRange(RC_ROW - 1, RC_COL, addValues.length, addValues[0].length).setValues(addValues.reverse());
 }
